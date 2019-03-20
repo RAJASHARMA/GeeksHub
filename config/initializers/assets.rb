674f369d472a29b07781de7f0a0ed4d2e8dd7366
SushantKumar29 <sushantkmr29@gmail.com>
2019-03-20 17:05:17 +0530

    Template Integrated

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,10 +2,25 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( ionicons.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( flaticon.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( jquery-3.2.1.min.js )
+Rails.application.config.assets.precompile += %w( jquery-migrate-3.0.0.js )
+Rails.application.config.assets.precompile += %w( popper.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( jquery.waypoints.min.js )
+Rails.application.config.assets.precompile += %w( jquery.stellar.min.js )
+Rails.application.config.assets.precompile += %w( main.js )
