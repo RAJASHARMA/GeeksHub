6a8a511772947bdd38c1ce9e1d2c34f3a52b9575
RAJASHARMA <rajasharmarkl92@gmail.com>
2019-05-14 11:36:30 +0530

    code refactor with user name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,16 @@ module ApplicationHelper
       end
     end
   end 
+
+
+  def get_user_name(profile)
+    if !profile.name.nil?
+      profile.name
+    elsif !profile.public_email.nil?
+      profile.public_email.split('@')[0]
+    else
+      profile.user.email.split('@')[0]
+    end
+  end
   
 end
